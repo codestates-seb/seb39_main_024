e3981099aff8ba06b1b6958647239a76cb15e8c1
sungmingt <qotjdals8313@naver.com>
2022-09-17 17:10:50 +0900

    REFACTOR : 조회 메서드에 readOnly 속성 추가

--- server/fly-away/src/main/java/com/codestates/flyaway/domain/member/service/MemberService.java
+++ server/fly-away/src/main/java/com/codestates/flyaway/domain/member/service/MemberService.java
@@ -16,12 +16,14 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Transactional(readOnly = true)
     public Member findById(long memberId) {
         return memberRepository.findById(memberId)
                 .orElseThrow(() -> new RuntimeException("회원 존재하지 않음"));   // Custom Exception 으로 대체
     }
 
     //특정 회원을 모든 운동기록과 함께 조회 (회원 프로필)
+    @Transactional(readOnly = true)
     public MemberProfileResponseDto findByIdFetch(long memberId) {
         Member findMember = memberRepository.findByIdFetch(memberId)
                 .orElseThrow(() -> new RuntimeException("회원 존재 x"));
