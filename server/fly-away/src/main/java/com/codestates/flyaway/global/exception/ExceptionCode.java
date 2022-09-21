7ccd45cfb09fd5b7ac51af9f9b91ba0e65d239b2
sungmingt <qotjdals8313@naver.com>
2022-09-21 14:18:01 +0900

    FEAT : 예외처리 파일 관련 오류코드 추가

--- server/fly-away/src/main/java/com/codestates/flyaway/global/exception/ExceptionCode.java
+++ server/fly-away/src/main/java/com/codestates/flyaway/global/exception/ExceptionCode.java
@@ -8,7 +8,9 @@ public enum ExceptionCode {
     IMAGE_NOT_FOUND(404, "존재하지 않는 이미지입니다."),
     EMAIL_ALREADY_EXISTS(409, "이미 존재하는 이메일입니다."),
     EMAIL_NOT_VALID(400, "유효하지 않은 이메일 형식입니다."),
-    PASSWORD_NOT_VALID(400, "유효하지 않은 비밀번호 형식입니다.");
+    PASSWORD_NOT_VALID(400, "유효하지 않은 비밀번호 형식입니다."),
+    FILE_NOT_FOUND(404, "파일이 존재하지 않습니다."),
+    FILE_DELETE_FAILED(404, "파일을 삭제에 실패했습니다.");
 
     @Getter
     private int status;
