7450d75203b3830db3636b47463f572cbf930154
sungmingt <qotjdals8313@naver.com>
2022-09-21 14:20:24 +0900

    REFACTOR : member와 memberImage 연관관계 주인 수정

--- server/fly-away/src/main/java/com/codestates/flyaway/domain/member/entity/Member.java
+++ server/fly-away/src/main/java/com/codestates/flyaway/domain/member/entity/Member.java
@@ -1,5 +1,6 @@
 package com.codestates.flyaway.domain.member.entity;
 
+import com.codestates.flyaway.domain.memberimage.MemberImage;
 import com.codestates.flyaway.domain.record.entity.Record;
 import com.codestates.flyaway.global.audit.Auditable;
 import lombok.AllArgsConstructor;
@@ -12,6 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import static javax.persistence.FetchType.*;
 import static javax.persistence.GenerationType.*;
 import static lombok.AccessLevel.*;
 
@@ -24,6 +26,10 @@ public class Member extends Auditable {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
+
+    @OneToOne(fetch = LAZY, cascade = CascadeType.PERSIST)   /////
+    @JoinColumn(name = "image_id")
+    private MemberImage memberImage;
 
     @OneToMany(mappedBy = "member")
     private List<Record> records = new ArrayList<>();
@@ -38,14 +44,16 @@ public class Member extends Auditable {
         this.password = password;
     }
 
+    public void setMemberImage(MemberImage memberImage) {
+        this.memberImage = memberImage;
+    }
+
     //==================
 
 
-    public void update(String name, String email, String password) {
+    public void update(String name, String password) {
         Optional.ofNullable(name)
                 .ifPresent(n -> this.name = n);
-        Optional.ofNullable(email)
-                .ifPresent(e -> this.email = e);
         Optional.ofNullable(password)
                 .ifPresent(p -> this.password = p);
     }
---
+++ server/fly-away/src/main/java/com/codestates/flyaway/domain/memberimage/MemberImage.java
@@ -0,0 +1,42 @@
+package com.codestates.flyaway.domain.memberimage;
+
+import com.codestates.flyaway.domain.member.entity.Member;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.*;
+import static lombok.AccessLevel.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+public class MemberImage {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @OneToOne(mappedBy = "memberImage")
+    private Member member;
+
+    private String fileOriName;
+    private String fileUrl;
+    private String fileName;
+
+    public MemberImage(String fileOriName, String fileUrl, String fileName) {
+        this.fileOriName = fileOriName;
+        this.fileUrl = fileUrl;
+        this.fileName = fileName;
+    }
+
+
+    //=====================
+
+    //연관관계
+    public void setMember(Member member) {
+        this.member = member;
+        member.setMemberImage(this);
+    }
+}
