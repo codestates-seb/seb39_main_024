2c8b8629d7e169db0f26012ee5b91226bd206ad3
BBO <46449090+slevme@users.noreply.github.com>
2022-10-05 14:38:07 +0900

    update baseURL

--- client/src/service/request.js
+++ client/src/service/request.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 // import https from 'https';
 
 const instance = axios.create({
-  baseURL: `https://flyaway.main024.shop/`,
+  baseURL: `https://server.main024.shop/`,
 });
 
 export default instance;
