e1c263236bb01d02bd66aedd46f80dc6d760887b
ian <onlordev@gmail.com>
2022-10-05 22:07:56 +0900

    fix navbar

--- client/src/containers/Navbar.jsx
+++ client/src/containers/Navbar.jsx
@@ -16,8 +16,6 @@ export default function Navbar({ path }) {
     alert('로그인 후 이용해주세요.');
   };
 
-  const notWriting = path !== '/posts/create' && path !== '/posts/edit';
-
   return (
     <nav
       className={`flex fixed mt-[64px] md:mt-[80px] h-10 md:h-12 sm:text-xs bg-white ${'border-y-[1px]'}`}
@@ -34,47 +32,39 @@ export default function Navbar({ path }) {
           </div>
         </div>
       )}
-      {notWriting && (
-        <>
-          {path.includes('/posts') && (
-            <div className="flex flex-row items-center justify-between w-screen">
-              <div>
-                <Button link="/posts" str="전체" />
-                <Button
-                  link="/posts/record"
-                  str="운동 기록"
-                  value="1"
-                  onClick={navHandler}
-                />
-                <Button
-                  link="/posts/meal"
-                  str="다이어트 식단"
-                  value="2"
-                  onClick={navHandler}
-                />
-                <Button
-                  link="/posts/free"
-                  str="자유"
-                  value="3"
-                  onClick={navHandler}
-                />
-              </div>
-              <div className="flex items-center">
-                <div className="mr-5">
-                  {isLogin && <Button link="/posts/create" str="글쓰기" />}
-                  {!isLogin && (
-                    <Button
-                      link="/login"
-                      str="글쓰기"
-                      onClick={loginPageHandler}
-                    />
-                  )}
-                </div>
-                <Search placeholder="글 검색하기" />
-              </div>
+      {path.includes('/posts') && (
+        <div className="flex flex-row items-center justify-between w-screen">
+          <div>
+            <Button link="/posts" str="전체" />
+            <Button
+              link="/posts/record"
+              str="운동 기록"
+              value="1"
+              onClick={navHandler}
+            />
+            <Button
+              link="/posts/meal"
+              str="다이어트 식단"
+              value="2"
+              onClick={navHandler}
+            />
+            <Button
+              link="/posts/free"
+              str="자유"
+              value="3"
+              onClick={navHandler}
+            />
+          </div>
+          <div className="flex items-center">
+            <div className="mr-5">
+              {isLogin && <Button link="/posts/create" str="글쓰기" />}
+              {!isLogin && (
+                <Button link="/login" str="글쓰기" onClick={loginPageHandler} />
+              )}
             </div>
-          )}
-        </>
+            <Search placeholder="글 검색하기" />
+          </div>
+        </div>
       )}
     </nav>
   );
