877bb9a126c2be9cedc3a6d6a9c38b4606db1ef0
ian <onlordev@gmail.com>
2022-10-07 19:53:31 +0900

    fix expired token

--- client/src/containers/components/ProfileIcon.jsx
+++ client/src/containers/components/ProfileIcon.jsx
@@ -6,6 +6,12 @@ export default function ProfileIcon() {
   const memberImg = useRecoilValue(memberImgState);
   const navigation = useNavigate();
 
+  if (memberImg === 'token') {
+    alert('로그인 시간이 만료되었습니다. 다시 로그인 해주세요.');
+    window.localStorage.clear();
+    window.location.replace('/login');
+  }
+
   return (
     <img
       className="m-2 w-[30px] h-[30px] rounded-full cursor-pointer"
